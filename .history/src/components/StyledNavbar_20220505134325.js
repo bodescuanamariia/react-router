3c546a55b82d7e179d7fad9c9ac94ae318e70801
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-05-05 13:47:28 +0300

    feat: add style to active link (NavLink)
    There are multiple ways to set up the active navlink: using active class, using inline css

---
+++ .history/src/components/StyledNavbar_20220505134325.js
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink
+        to="/"
+        style={({ isActive }) => {
+          return { color: isActive ? "red" : "grey" };
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink to="about">About</NavLink>
+      <NavLink to="products">Products</NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
