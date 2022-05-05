9cb812b8f6da5ae134894c25e98a6bb59326374c
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-05-05 13:35:29 +0300

    feat: share layout

--- .history/src/pages/Home_20220505133504.js
+++ .history/src/pages/Home_20220505133504.js
@@ -2,9 +2,11 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <section className="section">
-      <h2>Home page</h2>
-    </section>
+    <>
+      <section className="section">
+        <h2>Home page</h2>
+      </section>
+    </>
   );
 };
 export default Home;
--- .history/src/pages/SharedLayout_20220505133444.js
+++ .history/src/pages/SharedLayout_20220505133444.js
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <section className="section">
-      <h2>Home page</h2>
-    </section>
+    <>
+      <Navbar />
+      <Outlet />
+    </>
   );
 };
 export default Home;
